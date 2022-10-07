db24ecc815a701dd71331ec24f3bf533d763274a
Şevval Türkyılmaz <sevvalturkyilmazz@gmail.com>
2022-10-07 13:19:27 +0200

    navigationbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import TopBar from "./topbar/TopBar";
+
 function App() {
   return (
-    <div className="App">
-      blog app
-    </div>
+    <>
+    <TopBar/>
+ 
+    </>
   );
 }
 
---
+++ src/topbar/TopBar.jsx
@@ -0,0 +1,31 @@
+import "./topbar.css"
+
+export default function TopBar() {
+    return (
+        <div className="top">
+            <div className="topLeft">
+           
+            <i className="topIcon fa-brands fa-twitter"></i>
+            <i className="topIcon fa-brands fa-facebook"></i>
+            <i className="topIcon fa-brands fa-instagram"></i>
+            <i className="topIcon fa-brands fa-tiktok"></i>
+            </div>
+            <div className="topCenter">
+                <ul className="topList">
+                    <li className="topListItem">Home</li>
+                    <li className="topListItem">About Company</li>
+                    <li className="topListItem">Contact</li>
+                    <li className="topListItem">Write</li>
+                    <li className="topListItem">Logout</li>
+                </ul>
+
+            </div>
+            <div className="topRight"> 
+            <img src="" alt=""/>
+            <i class="topSearchIcon fa-solid fa-magnifying-glass"></i>
+            </div>
+            
+        </div>
+    )
+}
+
