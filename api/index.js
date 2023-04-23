f5e0470d50212605dc826849b8e9604f6fd8f948
Şevval Türkyılmaz <sevvalturkyilmazz@gmail.com>
2023-04-23 22:31:58 +0300

    authentication

--- api/index.js
+++ api/index.js
@@ -6,7 +6,7 @@ const authRoute = require("./routes/auth")
 
 
 dotenv.config();
-app.use(express.json())
+app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URL).then(console.log("connected to mongodb")).catch((err)=>console.log(err));
 
--- api/routes/auth.js
+++ api/routes/auth.js
@@ -5,14 +5,16 @@ const bcrypt=require("bcrypt");
 //Register
 router.post("/register",async(req,res) =>{
     try{
+        const salt=await bcrypt.genSalt(10);
+        const hashedPass= await bcrypt.hash(req.body.password,salt);
         const newUser = new User({
                 username:req.body.username,
                 email:req.body.email,
-                password:req.body.password
+                password:hashedPass
             });
             
         const user = await newUser.save();
-        res.status(500).json(err)
+        res.status(200).json(user)
     }
     catch(err){
         res.status(500).json(err);
@@ -21,5 +23,22 @@ router.post("/register",async(req,res) =>{
 });
 
 //Login
+
+router.post("/login",async(req,res)=>{
+    try{
+        const user= await User.findOne({username:req.body.username});
+        !user&&res.status(400).json("Wrong credentials.");
+  
+        const validated = await bcrypt.compare(req.body.password,user.password);
+        !validated && res.status(400).json("Wrong password");
+        const {password,...others}=user._doc;
+        res.status(200).json(others);
+        
+    }
+    catch(err){
+        res.status(500).json(err);
+    }
+
+});
 
 module.exports=router
